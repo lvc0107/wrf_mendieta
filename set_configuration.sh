d48e2f18f094db41e3e152a5b55141c45536e518
lvc0107 <miguelmn@gmail.com>
2017-01-29 16:42:10 -0300

    updated scripts

--- set_configuration.sh
+++ set_configuration.sh
@@ -3,21 +3,10 @@
 ################################# WRF Configuration ###################################
 ### Environment modules
 
-#module load mpi/mvapich2-cuda/2.1-gcc_4.9.2-cuda_7.0
-module load mpi/openmpi/1.8.4-gcc_4.9.2
-### Load:
-### 1) cuda/7.0     
-### 2) libs/gcc/4.9                   
-### 3) compilers/gcc/4.9
-### 4) mpi/mvapich2-cuda/2.1-gcc_4.9.2-cuda_7.0
-
-module load libs/netcdf-fortran/4.4.2-netcdf_4.3.3.1-gcc_4.9.2
-### 5) libs/hdf5/1.8.15-gcc_4.9.2
-### 6) libs/netcdf/4.3.3.1-gcc_4.9.2
-### 7) libs/netcdf-fortran/4.4.2-netcdf_4.3.3.1-gcc_4.9.2
+module load gcc/5
 
 export WRF_VERSION=WRF3.6.1                        #Tambien disponible WRF.3.8
-export WRF_BASE=$HOME/wrf_mendieta
+export WRF_BASE=$(pwd)
 export WRF_DIR=$WRF_BASE/$WRF_VERSION/WRFV3        # Directorio principal de WRF
 export WPS_DIR=$WRF_BASE/$WRF_VERSION/WPS          # Directorio principal del pre-procesador WPS
 export ARWPOST_DIR=$WRF_BASE/$WRF_VERSION/ARWpost  # Directorio principal del post-procesador ARWPost
