8e47d5cd4984ded964b7747253aaf27ee709756a
lvargas <miguelmnr@gmail.com>
2017-02-11 20:08:52 -0300

    improvent code

--- get_GFSX025_grib2.py
+++ get_GFSX025_grib2.py
@@ -3,54 +3,20 @@
 
 import datetime
 import urllib2
-import string
 import sys
 import os
 import time
-import re
-import shutil
-import tarfile
 
 #############################################################################
-#def chunk_report2(count, total):
-def chunk_report2():
+
+def chunk_report():
     bar_len = 60
-    #filled_len = int(round(bar_len * count / float(total)))
-    #percents = round(100.0 * count / float(total), 1)
     filled_len = 0
     percents = 0
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
 
     sys.stdout.write('[%s] %s%s ...\r' % (bar, percents, '%'))
     sys.stdout.flush() 
-        
-def chunk_report(bytes_so_far=0, chunk_size=0, total_size=0):
-   #percent = float(bytes_so_far) / total_size
-   #percent = round(percent*100, 2)
-   sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" %  (bytes_so_far, total_size, percent))
-
-   if bytes_so_far >= total_size:
-      sys.stdout.write('\n')
-
-def request(url, file):
-  
-    try:
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-    except urllib2.HTTPError, e:
-        print 'The server could not fulfill the request.'
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
-    sys.stdout.flush() 
-        
-def chunk_report(bytes_so_far=0, chunk_size=0, total_size=0):
-   #percent = float(bytes_so_far) / total_size
-   #percent = round(percent*100, 2)
-   sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" %  (bytes_so_far, total_size, percent))
-
-   if bytes_so_far >= total_size:
-      sys.stdout.write('\n')
 
 def request(url, file):
   
@@ -76,28 +42,19 @@ def request(url, file):
     else:
         w = open(file, 'w')
         w.write(response.read())
-        """
-        #total_size = len(response.read())
-        #total_size = int(total_size)
-        #bytes_so_far = 0
+
         chunk_size=8192
         while 1:
             chunk = response.read(chunk_size)
-            #bytes_so_far += len(chunk)
-            #print "chunk "  + chunk
             if not chunk:
                 break
-            #chunk_report(bytes_so_far, chunk_size, total_size)
-            chunk_report2()
-
-            #w.write(response.read())
+            chunk_report()
             w.write(chunk)
-        """
+
         response.close()
         w.close()
         return 0
 
-#############################################################################
 
 def download_grib_files(start_date, offset, grib2_dir):
 
@@ -112,21 +69,13 @@ def download_grib_files(start_date, offset, grib2_dir):
     DATE = INIDATE / 100
     # Instant(hour, UTC) of forecast start(analysis)
 
-    # FCI = int(sys.argv[1])# reads input on the calling line(example: "python get_GFS_grib2_slice.py 00")# FCI = input('Type UTC time of analysis (00, 06, 12, 18) ')# for request of manual input after launch
     FCI = INIDATE - DATE * 100
 
     # Archive tar - file creation option(ARCHIVE = True or ARCHIVE = False)
     ARCHIVE = False
 
-    # Directories where downloaded files are stored and(optionally) archived
-
-    ###DIR_CURRENT = os.getcwd()
-
     if ARCHIVE:
         DIR_ARCHIVE = DIR_OPER_DATA
-
-    # Domain(limited area) definition(geographical coordinates)
-
     LON_W = "-96"
     LON_E = "-15"
     LAT_N = "-10"
@@ -143,12 +92,8 @@ def download_grib_files(start_date, offset, grib2_dir):
 
     # Total forecast length( in hours) for which data are requested:
     NHOUR = int(offset)
-    # NHOUR = int(os.environ['HRUN'])
-
     # Interval in hours between two forecast times:
     DHOUR = 03
-    # DHOUR = int(os.environ['DHINPUT'])
-
     # Definiton of date( in required format)
     day = datetime.datetime.today()
     ### tomorrow = day + datetime.timedelta(days = 1)### yesterday = day + datetime.timedelta(days = -1)
@@ -176,7 +121,6 @@ def download_grib_files(start_date, offset, grib2_dir):
     #Definition of requested levels and parameters
 
     LEV_LIST = ["all"]
-    # PAR_LIST = ["TMAX"]
     PAR_LIST = ["HGT", "LAND", "PRES", "PRMSL", "RH", "SOILW", "SPFH", "TMP", "UGRD", "VGRD", "WEASD", "TSOIL"]
 
 
@@ -213,7 +157,6 @@ def download_grib_files(start_date, offset, grib2_dir):
     # Full list of requested files
 
     LIST_FILE_REMOTE = []
-    # LIST_FILE_LOCAL = []
     LIST_FLAG = []
     LIST_FILE_LOCAL_FIN = []
     NINST = NFILE_REQUESTED[0]
@@ -243,19 +186,11 @@ def download_grib_files(start_date, offset, grib2_dir):
         if ADGRID == "2.5":
             FILE_NAME_BASE = "?file=gfs.t" + FCIA + "z.pgrbf" + HFA + ".2p5deg.grib2"
         FILE_REMOTE = FILE_NAME_INI + FILE_NAME_BASE + LEVELS + PARAMETERS + FILE_NAME_DOMAIN + DIR_NAME
-        # FILE_LOCAL = "GFS_" + LEV_TYPE[ILEVTYPE] + "_" + day + FCIA + "+" + HFA2 + ".grib2"
         FILE_LOCAL_FIN = "GFS_" + day + FCIA + "+" + HFA2 + ".grib2"
         FLAG = 0
         LIST_FILE_REMOTE.append(FILE_REMOTE)
-        # LIST_FILE_LOCAL.append(FILE_LOCAL)# if ILEVTYPE == 0: LIST_FILE_LOCAL_FIN.append(FILE_LOCAL_FIN)
         LIST_FILE_LOCAL_FIN.append(FILE_LOCAL_FIN)
         LIST_FLAG.append(FLAG)
-
-        #print "********************************"
-        #print FILE_REMOTE
-        #print "********************************"
-        #print FILE_LOCAL_FIN
-        #print "********************************"
 
     NFILE = len(LIST_FLAG)
 
@@ -282,8 +217,6 @@ def download_grib_files(start_date, offset, grib2_dir):
                         icount = 0
                         while ierr != 0 and icount <= icountmax:
                             icount = icount + 1
-                            # The following prints on the sceen the entire text of the request
-                            #print "Request remote file: ", FILE_REMOTE
                             ierr = request(FILE_REMOTE, FILE_LOCAL)
                             print 'dowloading error= ', ierr
                             if ierr == 0: #successeful downloading
